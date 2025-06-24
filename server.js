e664bbb7116e6e302e72eb93b8d35387d2a32cf5
craigfahner <craig.fahner@gmail.com>
2025-06-24 13:52:22 -0400

    changed port configuration to be compatible with Heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const path = require("path");
 const express = require("express");
 const app = express();
 const server = require("http").Server(app);
-const port = 3000;
+//const port = 3000;
 
 const roomMax = 9;
 
@@ -20,9 +20,14 @@ const notes = [
 ];
 var activeNotes = [];
 
-server.listen(port, () => {
-  console.log("server is listening on port " + port);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
 });
+
+// server.listen(port, () => {
+//   console.log("server is listening on port " + port);
+// });
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public/index.html"));
